26a3309676afe3e8bdabc2720974f4f14464db32
Manchanda <smancha5@optum.com>
2022-01-09 15:16:16 +0000

    step 5

--- main.py
+++ main.py
@@ -111,11 +111,15 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                "main",
+                parameters={
+                    "mlflow_model":"random_forest_export:prod",
+                    "test_dataset":"test_data.csv:latest",
 
-            pass
+                },
+            )
 
 
 if __name__ == "__main__":
